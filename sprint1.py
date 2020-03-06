8a77b7f99dbbc66d18acdcc0b5f698f0585453a7
Mateus Santos Fonseca <mateus.santos.fsc@gmail.com>
2020-03-05 21:10:00 -0300

    Indentando query, adicionando export para csv sem formatacao

--- sprint1.py
+++ sprint1.py
@@ -1,20 +1,52 @@
 import requests
 import json
+import csv
+
+def executar_query_github(query):
+    request = requests.post('https://api.github.com/graphql', json = {'query': query}, headers = headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("A query falhou: {}. {}".format(request.status_code, query))
+
+def exportar_para_csv(data):
+    outputFile = open("repositorios.csv", 'w+')
+    for i, repositorio in enumerate(nodes):
+        outputFile.write("\n" + json.dumps(nodes[i]) + "\n")
+
 
 headers = {
     'Content-Type': 'application/json',
     'Authorization': 'bearer 08a7e4d6ae0ddc541b76206a37549dad5f20a80a',
 }
 
-data = '{"query": "{ search(query:\\"stars:>100\\", type:REPOSITORY, first:100){ nodes { ... on Repository {nameWithOwner url createdAt updatedAt pullRequests{ totalCount } releases{ totalCount } primaryLanguage{ name } todasIssues: issues{ totalCount } issuesFechadas: issues(states:CLOSED){ totalCount } } } } }"}'
+data = """
+{
+    search(query:"stars:>100", type:REPOSITORY, first:5) {
+        pageInfo {
+            hasNextPage
+            endCursor
+        }
+        nodes {
+            ... on Repository {
+                nameWithOwner
+                url
+                createdAt
+                updatedAt
+                pullRequests{ totalCount }
+                releases{ totalCount }
+                primaryLanguage{ name }
+                todasIssues: issues{ totalCount }
+                issuesFechadas: issues(states:CLOSED){ totalCount }
+            } 
+        } 
+    }
+}
+"""
 
-response = requests.post(
-	'https://api.github.com/graphql', headers=headers, data=data)
+response = executar_query_github(data)
 
-
-json_data = json.loads(response.text)
-
-nodes = json_data['data']['search']['nodes']
+nodes = response['data']['search']['nodes']
 
 # Formatando as datas dos nodes para ficar mais limpo (yyyy-mm-dd)
 for i, repositorio in enumerate(nodes):
@@ -22,4 +54,4 @@ for i, repositorio in enumerate(nodes):
     nodes[i]['updatedAt'] = nodes[i]['updatedAt'].split("T")[0]
     # print("\n", nodes[i]) # Printando apenas os nodes
 
-print (json_data)
+exportar_para_csv(response)
