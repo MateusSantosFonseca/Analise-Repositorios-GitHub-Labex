5400e5f9e7869d61687c5374cffed6ee5998bf02
Mateus Santos Fonseca <mateus.santos.fsc@gmail.com>
2020-03-06 03:35:24 -0300

    Sprint 2 finalizada, Finalizado logica paginacao e exportacao csv

--- sprint1.py
+++ sprint1.py
@@ -11,18 +11,24 @@ def executar_query_github(query):
 
 def exportar_para_csv(data):
     outputFile = open("repositorios.csv", 'w+')
-    for i, repositorio in enumerate(nodes):
-        outputFile.write("\n" + json.dumps(nodes[i]) + "\n")
+    for i, repositorio in enumerate(data):
+        outputFile.write("\n" + json.dumps(data[i]) + "\n")
 
+# Formatando as datas dos nodes para ficar mais limpo (yyyy-mm-dd)  
+def formatar_datas(response):
+    nodes = response['data']['search']['nodes']
+    for i, repositorio in enumerate(nodes):
+        nodes[i]['createdAt'] = nodes[i]['createdAt'].split("T")[0]
+        nodes[i]['updatedAt'] = nodes[i]['updatedAt'].split("T")[0]
 
 headers = {
     'Content-Type': 'application/json',
     'Authorization': 'bearer 08a7e4d6ae0ddc541b76206a37549dad5f20a80a',
 }
 
-data = """
+query = """
 {
-    search(query:"stars:>100", type:REPOSITORY, first:100){
+    search(query:"stars:>100", type:REPOSITORY, first:50 {placeholder}){
         pageInfo {
             hasNextPage
             endCursor
@@ -44,14 +50,39 @@ data = """
 }
 """
 
-response = executar_query_github(data)
+# O placeholder é responsável por substituir o cursor da próxima pagina (na primeira iteração ele é descartado)
+query_inicial = query.replace("{placeholder}", "")
 
-nodes = response['data']['search']['nodes']
 
-# Formatando as datas dos nodes para ficar mais limpo (yyyy-mm-dd)
-for i, repositorio in enumerate(nodes):
-    nodes[i]['createdAt'] = nodes[i]['createdAt'].split("T")[0]
-    nodes[i]['updatedAt'] = nodes[i]['updatedAt'].split("T")[0]
-    # print("\n", nodes[i]) # Printando apenas os nodes
+# Rodando a primeira vez 
+response = executar_query_github(query_inicial)
+formatar_datas(response) 
 
-exportar_para_csv(response)
+# 20 paginas * 50 repositórios por pagina = 1000 repositorios. Começa com um por já ter sido feita uma consulta
+
+quantidade_execucoes = 1 
+
+cursor_final_atual = response["data"]["search"]["pageInfo"]["endCursor"] # Atualiza o cursor
+
+has_next_page = response["data"]["search"]["pageInfo"]["hasNextPage"] # Atualiza se tem proxima página
+todos_resultados = response["data"]["search"]["nodes"]
+
+
+while (quantidade_execucoes < 20 and has_next_page):
+    finalQuery = query.replace("{placeholder}", ', after: "%s"' % cursor_final_atual) # Troca a query para possuir o cursor
+    
+    response = executar_query_github(finalQuery) # Executa novamente a função que consome a API
+    
+    formatar_datas(response) # Formata as datas
+    
+    todos_resultados += response["data"]["search"]["nodes"] # Incrementa o output de resultados
+    quantidade_execucoes += 1
+   
+     # Altera a proxima pagina
+    has_next_page = response["data"]["search"]["pageInfo"]["hasNextPage"] 
+
+    # Altera o cursor para o atual
+    cursor_final_atual = response["data"]["search"]["pageInfo"]["endCursor"] 
+
+
+exportar_para_csv(todos_resultados)
