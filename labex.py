d30ab34ad65dd3c5ecd4cde71453e39e7c0a50fc
Mateus Santos Fonseca <mateus.santos.fsc@gmail.com>
2020-03-06 03:54:40 -0300

    Sprint 2 finalizada

--- labex.py
+++ labex.py
@@ -9,21 +9,21 @@ def executar_query_github(query):
     else:
         raise Exception("A query falhou: {}. {}".format(request.status_code, query))
 
-def exportar_para_csv(data):
-    outputFile = open("repositorios.csv", 'w+')
-    for i, repositorio in enumerate(data):
-        outputFile.write("\n" + json.dumps(data[i]) + "\n")
-
 # Formatando as datas dos nodes para ficar mais limpo (yyyy-mm-dd)  
 def formatar_datas(response):
     nodes = response['data']['search']['nodes']
     for i, repositorio in enumerate(nodes):
         nodes[i]['createdAt'] = nodes[i]['createdAt'].split("T")[0]
         nodes[i]['updatedAt'] = nodes[i]['updatedAt'].split("T")[0]
+        
+def exportar_para_csv(data):
+    outputFile = open("output_repositorios_github.csv", 'w+')
+    for i, repositorio in enumerate(data):
+        outputFile.write(json.dumps(data[i]) + "\n")
 
 headers = {
     'Content-Type': 'application/json',
-    'Authorization': 'bearer 08a7e4d6ae0ddc541b76206a37549dad5f20a80a',
+    'Authorization': 'bearer SeuTokenAqui',
 }
 
 query = """
@@ -53,29 +53,27 @@ query = """
 # O placeholder é responsável por substituir o cursor da próxima pagina (na primeira iteração ele é descartado)
 query_inicial = query.replace("{placeholder}", "")
 
-
 # Rodando a primeira vez 
 response = executar_query_github(query_inicial)
-formatar_datas(response) 
-
-# 20 paginas * 50 repositórios por pagina = 1000 repositorios. Começa com um por já ter sido feita uma consulta
-
 quantidade_execucoes = 1 
 
-cursor_final_atual = response["data"]["search"]["pageInfo"]["endCursor"] # Atualiza o cursor
+formatar_datas(response) 
 
+# Começa com um por já ter sido feita uma consulta
+cursor_final_atual = response["data"]["search"]["pageInfo"]["endCursor"] # Atualiza o cursor
 has_next_page = response["data"]["search"]["pageInfo"]["hasNextPage"] # Atualiza se tem proxima página
 todos_resultados = response["data"]["search"]["nodes"]
 
-
+# 20 paginas * 50 repositórios por pagina = 1000 repositorios.
 while (quantidade_execucoes < 20 and has_next_page):
     finalQuery = query.replace("{placeholder}", ', after: "%s"' % cursor_final_atual) # Troca a query para possuir o cursor
     
-    response = executar_query_github(finalQuery) # Executa novamente a função que consome a API
+    response = executar_query_github(finalQuery)
     
-    formatar_datas(response) # Formata as datas
+    formatar_datas(response)
     
-    todos_resultados += response["data"]["search"]["nodes"] # Incrementa o output de resultados
+    # Incrementa o output de resultados
+    todos_resultados += response["data"]["search"]["nodes"] 
     quantidade_execucoes += 1
    
      # Altera a proxima pagina
@@ -83,6 +81,5 @@ while (quantidade_execucoes < 20 and has_next_page):
 
     # Altera o cursor para o atual
     cursor_final_atual = response["data"]["search"]["pageInfo"]["endCursor"] 
-
 
 exportar_para_csv(todos_resultados)
